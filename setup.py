79a1c0bd16520fe8771d66f9012d05d13fec5fed
Zach Taylor <zachtylr04@gmail.com>
2020-07-18 16:02:36 -0500

    Update release version in setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     "console_scripts": ['pyp=pyp.pyp:main']
   },
   scripts=['./pyp-runner.py'],
-  version = '0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'A pip wrapper that writes requirements.txt automatically',
   author = 'Zach Taylor',
   author_email = 'zachdtaylor.b@gmail.com',
   url = 'https://github.com/zachtylr21/pyp',
-  download_url = 'https://github.com/zachtylr21/pyp/archive/v0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/zachtylr21/pyp/archive/v0.0.2.tar.gz',    # I explain this later on
   keywords = ['pip', 'python', 'package', 'requirements', 'dependency'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
