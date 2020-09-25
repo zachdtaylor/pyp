fa0c63b3e01275649f88ea4f0e0d9d8ed9d4c70d
Zach Taylor <zachtylr04@gmail.com>
2020-09-24 21:52:22 -0500

    Run autopep8 on all files

--- pyp/pyp.py
+++ pyp/pyp.py
@@ -3,45 +3,48 @@ import subprocess
 
 
 def pip_install(package_names):
-  p = subprocess.Popen(
-    'pip install ' + ' '.join(package_names),
-    shell=True
-  )
-  p.wait()
+    p = subprocess.Popen(
+        'pip install ' + ' '.join(package_names),
+        shell=True
+    )
+    p.wait()
+
 
 def pip_freeze(package_names):
-  patterns = "'" + '==\|'.join(package_names) + "'"
-  output = subprocess.check_output(
-    'pip freeze | grep ' + patterns + '==',
-    shell=True
-  ).decode('ascii')
-  return output
+    patterns = "'" + '==\|'.join(package_names) + "'"
+    output = subprocess.check_output(
+        'pip freeze | grep ' + patterns + '==',
+        shell=True
+    ).decode('ascii')
+    return output
+
 
 def write_requirements(packages):
-  for pkg in packages.split('\n')[:-1]: # Skip the '' at the end
-    while True:
-      try:
-        with open('requirements.txt', 'r+') as f:
-          for line in f:
-            if pkg in line:
-              break
-          else:
-            f.write(pkg + '\n')
-      except FileNotFoundError:
-        open('requirements.txt', 'w').close()
-      else:
-        break
+    for pkg in packages.split('\n')[:-1]:  # Skip the '' at the end
+        while True:
+            try:
+                with open('requirements.txt', 'r+') as f:
+                    for line in f:
+                        if pkg in line:
+                            break
+                    else:
+                        f.write(pkg + '\n')
+            except FileNotFoundError:
+                open('requirements.txt', 'w').close()
+            else:
+                break
+
 
 def main():
-  if sys.argv[1] == 'install':
-    package_names = sys.argv[2:]
-    pip_install(package_names)
-    installed_packages = pip_freeze(package_names)
-    write_requirements(installed_packages)
-  else:
-    print(
-      """
+    if sys.argv[1] == 'install':
+        package_names = sys.argv[2:]
+        pip_install(package_names)
+        installed_packages = pip_freeze(package_names)
+        write_requirements(installed_packages)
+    else:
+        print(
+            """
       pyp currently only supports install.
       If you want to contribute, visit https://github.com/zachtylr21/pyp
       """
-    )
+        )
--- setup.py
+++ setup.py
@@ -2,28 +2,28 @@ from setuptools import setup, find_packages
 
 
 setup(
-  name = 'pyp-manager',
-  packages = find_packages(), 
-  entry_points = {
-    "console_scripts": ['pyp=pyp.pyp:main']
-  },
-  scripts=['./pyp-runner.py'],
-  version = '0.0.2',
-  license='MIT',
-  description = 'A pip wrapper that writes requirements.txt automatically',
-  author = 'Zach Taylor',
-  author_email = 'zachdtaylor.b@gmail.com',
-  url = 'https://github.com/zachtylr21/pyp',
-  download_url = 'https://github.com/zachtylr21/pyp/archive/v0.0.2.tar.gz',
-  keywords = ['pip', 'python', 'package', 'requirements', 'dependency'],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='pyp-manager',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ['pyp=pyp.pyp:main']
+    },
+    scripts=['./pyp-runner.py'],
+    version='0.0.2',
+    license='MIT',
+    description='A pip wrapper that writes requirements.txt automatically',
+    author='Zach Taylor',
+    author_email='zachdtaylor.b@gmail.com',
+    url='https://github.com/zachtylr21/pyp',
+    download_url='https://github.com/zachtylr21/pyp/archive/v0.0.2.tar.gz',
+    keywords=['pip', 'python', 'package', 'requirements', 'dependency'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
