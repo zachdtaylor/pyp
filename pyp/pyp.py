e12b2cb16733dad08d81de84c301c3e771b66314
Zach Taylor <zachtylr04@gmail.com>
2020-10-24 00:26:13 -0500

    Add and run black formatter

--- pyp/pyp.py
+++ pyp/pyp.py
@@ -3,50 +3,52 @@ import subprocess
 import re
 from collections import OrderedDict
 
-REQUIREMENTS_FILE_NAME = 'requirements.txt'
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+
 
 def pip_install(package_names):
-    p = subprocess.Popen(
-        'pip install ' + ' '.join(package_names),
-        shell=True
-    )
+    p = subprocess.Popen("pip install " + " ".join(package_names), shell=True)
     p.wait()
+
 
 def sort_entries(entries):
     lowercase_list = [entry.lower() for entry in entries]
     return sorted(lowercase_list)
 
+
 def pip_freeze(package_names):
-    patterns = "'" + '==\|'.join(package_names) + "=='"
-    patterns = re.sub(r'[-_]', '[-_]', patterns)
+    patterns = "'" + "==\|".join(package_names) + "=='"
+    patterns = re.sub(r"[-_]", "[-_]", patterns)
     output = subprocess.check_output(
-        'pip freeze | grep -i ' + patterns,
-        shell=True
-    ).decode('ascii')
+        "pip freeze | grep -i " + patterns, shell=True
+    ).decode("ascii")
     return output
+
 
 def get_existing_entries():
     entries = []
     try:
-        with open(REQUIREMENTS_FILE_NAME, 'r') as file:
-            entries = [entry.rstrip('\n') for entry in file]
+        with open(REQUIREMENTS_FILE_NAME, "r") as file:
+            entries = [entry.rstrip("\n") for entry in file]
     except FileNotFoundError:
-        open(REQUIREMENTS_FILE_NAME, 'w').close()
-    
+        open(REQUIREMENTS_FILE_NAME, "w").close()
+
     return entries
+
 
 def write_requirements(packages):
     existing_entries = get_existing_entries()
-    new_entries = packages.split('\n')[:-1] # Skip the '' at the end
-    combined_entries = list(OrderedDict.fromkeys (existing_entries + new_entries))
+    new_entries = packages.split("\n")[:-1]  # Skip the '' at the end
+    combined_entries = list(OrderedDict.fromkeys(existing_entries + new_entries))
     sorted_entries = sort_entries(combined_entries)
 
-    file_contents = '\n'.join(sorted_entries) + '\n' # For the last entry
-    with open(REQUIREMENTS_FILE_NAME, 'w') as file:
+    file_contents = "\n".join(sorted_entries) + "\n"  # For the last entry
+    with open(REQUIREMENTS_FILE_NAME, "w") as file:
         file.write(file_contents)
 
+
 def main():
-    if sys.argv[1] == 'install':
+    if sys.argv[1] == "install":
         package_names = sys.argv[2:]
         pip_install(package_names)
         installed_packages = pip_freeze(package_names)
--- tests/test_all.py
+++ tests/test_all.py
@@ -3,14 +3,17 @@ from unittest.mock import patch
 from pyp.pyp import pip_freeze, sort_entries
 
 
-@patch('subprocess.check_output')
+@patch("subprocess.check_output")
 def test_pip_freeze_search_patterns(mock_check_output):
-    pip_freeze(['plain', 'with-hyphen', 'with_underscore', 'this-has_both'])
+    pip_freeze(["plain", "with-hyphen", "with_underscore", "this-has_both"])
     mock_check_output.assert_called_once()
-    mock_check_output.call_args[0][0] == "pip freeze | grep -i 'plain==\\|with[-_]hyphen==\\|with[-_]underscore==\\|this[-_]has[-_]both=='"
+    mock_check_output.call_args[0][
+        0
+    ] == "pip freeze | grep -i 'plain==\\|with[-_]hyphen==\\|with[-_]underscore==\\|this[-_]has[-_]both=='"
+
 
 def test_package_name_sorting_handles_irregular_names():
-    packages = ['Django-7.5', 'abc-33', 'rANndom_44', 'pip']
-    expected_output = ['abc-33', 'django-7.5', 'pip', 'ranndom_44']
+    packages = ["Django-7.5", "abc-33", "rANndom_44", "pip"]
+    expected_output = ["abc-33", "django-7.5", "pip", "ranndom_44"]
     sorted_packages = sort_entries(packages)
-    ''.join(sorted_packages) == ''.join(expected_output)
+    "".join(sorted_packages) == "".join(expected_output)
